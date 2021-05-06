224a5e6ae8dce58298cdb4b6c49afe26485e95a5
Ed Sants <edward.sants@googlemail.com>
2021-05-06 19:25:00 +0100

    added delete function to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,10 @@
 class ListsController < ApplicationController
-  # before_action :find_list, only: [:show]
+  before_action :find_list, only: [:show, :destroy]
   def index
     @lists = List.all
   end
 
-  def show
-    @list = List.find(params[:id])
-  end
+  def show; end
 
   def new
     @list = List.new
@@ -21,13 +19,18 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
   private
 
   def list_params
     params.require(:list).permit(:name, :photo)
   end
 
-  # def find_list
-  #   @list = List.find(params[:id])
-  # end
+  def find_list
+    @list = List.find(params[:id])
+  end
 end
